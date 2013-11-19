487c44300401b043319a7a2e6a1e00641298f7ea
Colin Harrower <corr@ceh.ac.uk>
2013-11-19 14:59:21 +0000

    Percentile plot complete but not suitable

--- R/comb_dist_alt_map.r
+++ R/comb_dist_alt_map.r
@@ -1,14 +1,9 @@
+
 # Simply 
-percentile_plot = function(x, percentile = seq(0,100,by=10), col = "red", bar_width = 10, show_lengend =  FALSE, ...){
+percentile_plot = function(x, percentile = seq(10,100,by=10), col = "red", bar_width = 10, show_legend =  FALSE, ...){
 	# Determine probabilities needed for quantile to determine values
-		upp_per  = 0 + (percentile/2)
-		low_per = 100 - (percentile/2)
-	# If upp_per == low_per for any pair then remove
-		rm_inds = which(upp_per == low_per)
-		if(length(rm_inds) > 0){
-			upp_per = upp_per[-rm_inds]
-			low_per = low_per[-rm_inds]
-		}
+		low_per  = (100-percentile)/2
+		upp_per = 100 - low_per
 	# Combine vectors and convert from percent to proportion
 		full_prob = sort(c(upp_per, low_per))/100
 	# Get percentiles
@@ -17,7 +12,14 @@ percentile_plot = function(x, percentile = seq(0,100,by=10), col = "red", bar_wi
 	# Get colour spectrum
 		band_cols = colorRampPalette(c("white",col))(n_per/2)
 	# Setup initial plot
-		plot(x = rep(1,length(x)), y = x, xlim = c(1,bar_width), type="n", xaxt = "n", xlab = NA, ylab = NA, bty = "n", ...)
+		# If legend to be show need to extend axis to make room for legend
+		if(show_legend){
+			legend_width = bar_width
+			xlim = c(1,bar_width + legend_width)
+		} else {
+			xlim = c(1,bar_width)
+		}
+		plot(x = rep(1,length(x)), y = x, xlim = xlim, type="n", xaxt = "n", xlab = NA, ylab = NA, bty = "n", ...)
 	# Add first rectangle for all data (the only one with an outline)
 	# Loop through other percentiles and add to plot
 	for(i in 2:(n_per/2)){
@@ -26,8 +28,10 @@ percentile_plot = function(x, percentile = seq(0,100,by=10), col = "red", bar_wi
 	# Add final all data band (so that outline border goes over bands (fill is transparent so can see other bands underneath)
 	rect(1,percentiles[1],bar_width,percentiles[n_per-(1-1)])
 	# Add legend if requested
-	if(show_legend = TRUE){
-	
+	if(show_legend){
+		leg_val = rev(paste(percentile,"%", sep=""))
+		#par(family = "mono")
+		legend("right",legend = format(leg_val, justify = "right"), fill = band_cols, border = c("black",rep(NA,length(band_cols)-1)), bty = "n")
 	}
 }
 
