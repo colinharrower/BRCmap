bac57dea5577b778a717bdddac2b41fbeae744ae
Colin Harrower <corr@ceh.ac.uk>
2014-07-22 11:59:14 +0100

    Minor bug fix: fixes a couple of bugs in full_output part of helmert_trans function (bug found by Tom August), see issue #7

--- R/helmert_trans.R
+++ R/helmert_trans.R
@@ -30,7 +30,7 @@ function(x, y, z, trans = "OSNItoOSGB", trans_params = NULL, full_output = FALSE
 		
 	# Setup object to hold output data (x,y,z)
 		if(full_output){
-			ret_obj = data.frame(org_x = x, org_y = y, org_z = z, TRANS = Datum, x = rep(NA, len_x), y = rep(NA, len_x), y = rep(NA, len_x), stringsAsFactors = FALSE)
+			ret_obj = data.frame(org_x = x, org_y = y, org_z = z, TRANS = trans, x = rep(NA, len_x), y = rep(NA, len_x), z = rep(NA, len_x), stringsAsFactors = FALSE)
 		} else {
 			ret_obj = data.frame(x = rep(NA, len_x), y = rep(NA, len_x), z = rep(NA, len_x), stringsAsFactors = FALSE)
 		}
