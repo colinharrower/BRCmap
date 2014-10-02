f4d894420aca9e7bb4a67aa6d01923eb36029ea3
Colin Harrower <corr@ceh.ac.uk>
2014-10-02 18:23:36 +0100

    A rewrite of OSgridReprojection function to allow it to deal with multiple reprojections at the same time

--- R/OSgridReprojection.R
+++ R/OSgridReprojection.R
@@ -7,31 +7,49 @@ function(Easting, Northing, org_grid = "OSNI", out_grid = "OSGB", full_output = 
 		}
 		
 	# Setup object to hold output variable
-		if(full_output){
-			ret_obj = data.frame(ORG_EASTING = Easting, ORG_NORTHING = Northing, ORG_GRID = org_grid, NEW_GRID = out_grid, EASTING = rep(NA, len_east), NORTHING = rep(NA, len_east), stringsAsFactors = FALSE)
-		} else {
-			ret_obj = data.frame(EASTING = rep(NA, len_east), NORTHING = rep(NA, len_east), stringsAsFactors = FALSE)
+		ret_obj = data.frame(ORG_EASTING = Easting, ORG_NORTHING = Northing, ORG_GRID = org_grid, NEW_GRID = out_grid, EASTING = rep(NA, len_east), NORTHING = rep(NA, len_east), stringsAsFactors = FALSE)
+		
+		
+	# Where org_grid == out_grid then copy across
+		inds = which(ret_obj$ORG_GRID == ret_obj$NEW_GRID)
+		if(length(inds) > 0){
+			ret_obj[inds,c("EASTING","NORTHING")] = ret_obj[inds,c("ORG_EASTING","ORG_NORTHING")]
 		}
 		
-	# Determine Lat/Lon (Original projection)
-		org_latlon = OSGridstoLatLong(Easting, Northing, org_grid)
-	# Determine Cartesian (Original projection)
-		org_cart = LatLong_Cartesian(org_latlon$LATITUDE, org_latlon$LONGITUDE, gsub("WGS84","UTM30", org_grid))
-	# Apply Helmert transformation to convert orginal projections to WGS84 (unless already WGS84/UTM30)
-	if(!org_grid %in% c("WGS84","UTM30")){
-		helm_tran = helmert_trans(x =org_cart$x, y = org_cart$y, z = org_cart$z, trans = paste(org_grid,"toWGS84", sep=""))
-	} else {
-		helm_tran = org_cart
+	# Determine which grids will need reprojected
+		inds = which(ret_obj$ORG_GRID != ret_obj$NEW_GRID)
+		if(length(inds) > 0){
+			# Determine Lat/Lon (Original projection)
+				org_latlon = OSGridstoLatLong(ret_obj$ORG_EASTING[inds], ret_obj$ORG_NORTHING[inds], ret_obj$ORG_GRID[inds])
+			# Determine Cartesian (Original projection)
+				org_cart = LatLong_Cartesian(org_latlon$LATITUDE, org_latlon$LONGITUDE, gsub("WGS84","UTM30", ret_obj$ORG_GRID[inds]))
+				# Add old and new grids to org_cart
+				org_cart[,c("ORG_GRID","NEW_GRID")] = ret_obj[inds,c("ORG_GRID","NEW_GRID")]
+			# First need to convert all to WGS84 as only have params for required conversion via WGS84 so which which org_grid not WGS84/UTM30
+				new_inds = which(!org_cart$ORG_GRID %in% c("WGS84","UTM30"))
+				
+				if(length(new_inds) > 0){
+					# For these gridrefs replace org_cart with tranformed values
+						org_cart[new_inds,c("x","y","z")] = helmert_trans(x =org_cart$x[new_inds], y = org_cart$y[new_inds], z = org_cart$z[new_inds], trans = paste(org_cart$ORG_GRID[new_inds],"toWGS84", sep=""))
+				}
+			# If out_grid not WGS84/UTM30 then need to convert all Cartesian coords to output grid
+				new_inds = which(!org_cart$NEW_GRID %in% c("WGS84","UTM30"))
+				
+				if(length(new_inds)  > 0){
+					org_cart[new_inds,c("x","y","z")] = helmert_trans(x =org_cart$x[new_inds], y = org_cart$y[new_inds], z = org_cart$z[new_inds], trans = paste("WGS84to",org_cart$NEW_GRID[new_inds], sep=""))
+				}
+			# Convert Cartesian coordiantes to Latitude/Longitude (New projection)
+				out_latlon = Cartesian_LatLong(org_cart$x, org_cart$y, org_cart$z, out_grid)
+			# Now covert lat lons to Easting & Northing (new projection)
+				out_en = LatLongtoOSGrids(out_latlon$LATITUDE, out_latlon$LONGITUDE, out_grid)
+			# Finally write values to output data.frame
+				ret_obj[inds,c("EASTING","NORTHING")] = out_en[,c("EASTING","NORTHING")]
+		}
+	
+	# If full_output = FALSE then remove extra columns
+	if(!full_output){
+		ret_obj = ret_obj[,c("EASTING","NORTHING")]
 	}
-	# If out_grid is not WGS84 or WGS84 elipsoid based (UTM30) then perform secondary convertion to covert WGS84 to out_grid
-	if(!out_grid %in% c("WGS84","UTM30")){
-		helm_tran = helmert_trans(x = helm_tran$x, y = helm_tran$y, z = helm_tran$z, trans = paste("WGS84to",out_grid, sep=""))
-	}
-	# Convert Cartesian coordinates to Latitude/Longitude (New projection)
-		out_latlon = Cartesian_LatLong(helm_tran$x, helm_tran$y, helm_tran$z, out_grid)
-	# Convert Latitude/Longitude to Eastings and Northings (New projection)
-		out_en = LatLongtoOSGrids(out_latlon$LATITUDE, out_latlon$LONGITUDE, out_grid)
-	# Write to output variable
-		ret_obj[,c("EASTING","NORTHING")] = data.frame(out_en$EASTING, out_en$NORTHING)
+		
 	return(ret_obj)
 }
