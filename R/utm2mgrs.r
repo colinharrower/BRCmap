1e429b27cb27f434c01104378fb552045d6548f3
Colin Harrower <corr@ceh.ac.uk>
2015-06-01 17:37:18 +0100

    modified utm2mgrs script to vectorise the code and allow a vector of utm strings to be processed.

--- R/utm2mgrs.r
+++ R/utm2mgrs.r
@@ -1,28 +1,35 @@
 utm2mgrs = function(utm_str, output_type = "full_gr"){
+	# Setup object to store output
+		zone = rep(NA, length(utm_str))
+		band = rep(NA, length(utm_str))
+		easting = rep(NA, length(utm_str))
+		northing = rep(NA, length(utm_str))
+		l_e = rep(NA, length(utm_str))
+		l_n = rep(NA, length(utm_str))
+		no_dec = rep(NA, length(utm_str))
+	
 	# Define letters for e100km & n100km
 		lets_e100km = LETTERS[-c(9,15)]
 		lets_n100km = c(LETTERS[-c(9,15, 23:26)], LETTERS[c(6:8,10:14,16:22,1:5)])
 	
-	# Convert to upper case if not already
-		utm_str = toupper(utm_str)
 	# Check UTM string matches expected pattern
-	if(!grepl("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$",utm_str)){
-		stop("ERROR: supplied string does not match expected UTM format, e.g. 30U 421937.838, 5564409.771")
+	bad_inds = which(!grepl("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$",utm_str))
+	if(length(bad_inds) > 0){
+		warning("One or more of the supplied strings were not recognised as UTM coordinates")
 	}
 
 	# Split UTM str into components
-		zone = gsub("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$","\\1", utm_str)
-		band = gsub("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$","\\2", utm_str)
-		easting = as.numeric(gsub("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$","\\3", utm_str))
-		northing = as.numeric(gsub("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$","\\5", utm_str))
+		zone[-bad_inds] = gsub("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$","\\1", utm_str[-bad_inds])
+		band[-bad_inds] = gsub("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$","\\2", utm_str[-bad_inds])
+		easting[-bad_inds] = gsub("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$","\\3", utm_str[-bad_inds])
+		northing[-bad_inds] = gsub("^([[:digit:]]{1,2})[ ]?([[:alpha:]]{1})[ ]?([[:digit:]]*([.][[:digit:]]*)?)[ ,]{1,2}(([[:digit:]]*([.][[:digit:]]*)?))$","\\5", utm_str[-bad_inds])
 		
 		# Determine decimal precision of easting & northing values
-			no_dec = max(c(nchar(easting) - nchar(floor(easting))-1, nchar(northing) - nchar(floor(northing))-1))
-			if(no_dec < 0){
-				no_dec = 0
-			}
-		
-		
+			no_dec[-bad_inds] = pmax(nchar(gsub("^([[:digit:]]+)([.]([[:digit:]]*))?$", "\\3", easting[-bad_inds])), nchar(gsub("^([[:digit:]]+)([.]([[:digit:]]*))?$", "\\3", northing[-bad_inds])))	
+	
+	# Converting easting & northing to numeric vector
+		easting = as.numeric(easting)
+		northing = as.numeric(northing)
 	
 	# Determine easting & northing letters
 		# Easting
@@ -37,10 +44,13 @@ utm2mgrs = function(utm_str, output_type = "full_gr"){
 	# Create output string
 	if(tolower(output_type) == "full_gr"){
 		ret_obj = sprintf("%s%s%s%s%05.0f%05.0f",zone,band,l_e,l_n,floor(east_100km),floor(north_100km))
+		ret_obj[bad_inds] = NA
 	} else if(tolower(output_type) == "split_gr"){
 		ret_obj = data.frame(GZD = paste(zone,band,sep=""), GRIDREF = sprintf("%s%s%05.0f%05.0f",l_e,l_n,floor(east_100km),floor(north_100km)))
+		ret_obj[bad_inds,] = c(NA,NA)
 	} else if(tolower(output_type) == "atomised"){
 		ret_obj = data.frame(ZONE = zone, BAND = band, SQ100KM_ID = paste(l_e,l_n, sep=""), EASTING = round(east_100km,no_dec), NORTHING = round(north_100km, no_dec))
+		ret_obj$SQ100KM_ID[bad_inds] = NA
 	} else {
 		stop("Unknown output type: valid options are 'full_gr', 'split_gr', or 'atomised'")
 	}
