bc419bd9803cc280708d72044d5b31bb5af45e2f
Colin Harrower <corr@ceh.ac.uk>
2015-06-03 10:58:57 +0100

    Minor modification: modified gr_det_country to correctly identify MGRS grid reference types

--- R/gr_det_country.r
+++ R/gr_det_country.r
@@ -1,18 +1,18 @@
 gr_det_country = function(gridref){
 	# Create variable to store output
 	cty_out = rep(NA, length(gridref))
-	
-  
-	"(^[[:upper:]]{1,2}[[:digit:]]{2}([[:upper:]]?|[[:upper:]]{2})$)|(^[[:upper:]]{1,2}[[:digit:]]{2,}$)"
   
 	# Find British Gridrefs
-	cty_out[grepl('(^[[:upper:]]{2}[[:digit:]]{2}([[:upper:]]?|[[:upper:]]{2})$)|(^[[:upper:]]{2}[[:digit:]]{2,}$)',gridref) & !grepl('^(WA)|(WV)',gridref)] = "OSGB"
+	cty_out[grepl("^[[:upper:]]{1,2}[[:digit:]]{2,}([[:upper:]]?|[[:upper:]]{2})?$",gridref) & !grepl("^(WA)|(WV)",gridref)] = "OSGB"
 	
 	# Find Irish Gridrefs
-	cty_out[grepl('(^[[:upper:]]{1}[[:digit:]]{2}([[:upper:]]?|[[:upper:]]{2})$)|(^[[:upper:]]{1}[[:digit:]]{2,}$)',gridref)] = "OSNI"
+	cty_out[grepl("^[[:upper:]]{1}[[:digit:]]{2,}([[:upper:]]?|[[:upper:]]{2})?$",gridref)] = "OSNI"
 	
 	# Find Channel Islands Gridrefs
-	cty_out[grepl('(^(WA)|(WV)[[:digit:]]{2}([[:upper:]]?|[[:upper:]]{2})$)|(^(WA)|(WV)[[:digit:]]{2,}$)',gridref)] = "UTM30"
+	cty_out[grepl("^(WA)|(WV)[[:digit:]]{2}([[:upper:]]?|[[:upper:]]{2})?$)",gridref)] = "UTM30"
+	
+	# Find MGRS grid reference types
+	cty_out[grepl("^[[:digit:]]{1,2}[[:upper:]]{1}[[:upper:]]{1,2}[[:digit:]]{2,}([[:upper:]]?|[[:upper:]]{2})?$", gridref)] = "MGRS"
 	
 	# Return output object
 	return(cty_out)
