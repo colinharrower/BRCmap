07f030c565ab661e26d3298cd8faf36774ccf8e2
Colin Harrower <corr@ceh.ac.uk>
2015-06-03 09:59:40 +0100

    Minor modification: modified fmt_gridref to deal with UTM grid references

--- R/fmt_gridref.r
+++ R/fmt_gridref.r
@@ -7,12 +7,12 @@ fmt_gridref = function(gridref, gr_fmt = NULL){
 		gridref = gsub("[[:space:][:cntrl:][:punct:]]", "", gridref)
 	# Check that gridref conforms to grid reference pattern after removals
 		# Get indices of gridrefs which are in a valid format
-			gr_inds = which(grepl("^[[:upper:]]{1,2}[[:digit:]]{2,}([[:upper:]]?|[[:upper:]]{2})$", gridref))
+			gr_inds = which(grepl("^([[:digit:]]{1,2}[[:upper:]]{1})?[[:upper:]]{1,2}[[:digit:]]{2,}([[:upper:]]?|[[:upper:]]{2})$", gridref))
 		# Copy valid gridrefs to output object
 			gr_out[gr_inds] = gridref[gr_inds]
-	# Extract components where gr_fmt is not NULL (1 = Whole gridref minus tet/quad codes, 2 = Inital letter(s), 3 = Digits only, 4 = Tetrad/Quad only, 5 = Tetrad only, 6 = Quadrant only)
+	# Extract components where gr_fmt is not NULL (1 = Whole gridref minus tet/quad codes inc utm zone&band if present,2 = UTM zone & band, 3 = whole gridref minus tet/quad codes & utm zone & band, 4 = Inital letter(s), 5 = Digits only, 6 = Tetrad/Quad only, 7 = Tetrad only, 8 = Quadrant only)
 	if(!is.null(gr_fmt)){
-		gr_out = gsub("^(([[:upper:]]{1,2})([[:digit:]]{2,}))(([[:upper:]]?)|([[:upper:]]{2}))$", paste("\\",gr_fmt, sep=""), gr_out)
+		gr_out = gsub("^(([[:digit:]]{1,2}[[:upper:]]{1})?(([[:upper:]]{1,2})([[:digit:]]{2,})))(([[:upper:]]?)|([[:upper:]]{2}))$", paste("\\",gr_fmt, sep=""), gr_out)
 	}
 	# Return formatted gridref
 		return(gr_out)
